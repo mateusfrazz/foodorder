6ddded6e667265f625667b38529f3ea60a7def51
mazeus <mateusfrazao12345@gmail.com>
2025-04-12 15:16:12 -0300

    finalizand o ngFor para renderizar as imagens na page home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FoodService } from '../../services/food/food.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -14,5 +17,4 @@ export class HomeComponent {
    ngOnInit(): void {
     this.foods = this.fs.getAll();
    }
-
 }
