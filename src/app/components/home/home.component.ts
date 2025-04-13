eb3bc2688fbfa3eb1baa2c8d247917af545ea0f3
mazeus <mateusfrazao12345@gmail.com>
2025-04-13 15:52:02 -0300

    add: criado um novo component para gerar os cards na tela

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoodService } from '../../services/food/food.service';
+import { Foods } from '../../shared/models/food';
 
 @Component({
   selector: 'app-home',
   imports: [
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-   foods:string[]=[];
+   foods:Foods[]=[];
+   isFavorite = false;
    constructor(private fs:FoodService){}
-
+  
    ngOnInit(): void {
     this.foods = this.fs.getAll();
+   }
+
+   toogleFavorite() {
+    this.isFavorite = !this.isFavorite
    }
 }
