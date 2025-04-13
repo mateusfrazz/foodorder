db1d6cf627e354bafe9d8b442d685820434fd360
mazeus <mateusfrazao12345@gmail.com>
2025-04-13 16:05:25 -0300

    Definindo a interface para tipar os dados

---
+++ src/app/Interfaces/Produto.ts
@@ -0,0 +1,11 @@
+export interface Produto { 
+    id:number,
+    price:number,
+    name:string;
+    favorite:boolean
+    star:number 
+    tags:string[];
+    imageUrl:string;
+    cookTime: string;
+    origins:string[];
+}
