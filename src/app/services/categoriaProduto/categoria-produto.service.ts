0480b9c80319ebd774716b71a272b7e9fa74433d
mazeus <mateusfrazao12345@gmail.com>
2025-04-24 16:49:11 -0300

    criado o service par a retornar as categorias do json server

--- src/app/services/categoriaProduto/categoria-produto.service.ts
+++ src/app/services/categoriaProduto/categoria-produto.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Produto } from '../../Interfaces/Produto';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaProdutoService {
+  private apiUrl = 'http://localhost:3000/categoria'
+  constructor( private http: HttpClient) { }
 
-  constructor() { }
+  getCategoriaProduto() :Observable<Produto[]>{
+    return this.http.get<Produto[]>(this.apiUrl)
+  }
 }
