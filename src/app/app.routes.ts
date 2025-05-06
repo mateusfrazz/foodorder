56db6ed62b234128c072ca09476b4b6a96410fd1
mazeus <mateusfrazao12345@gmail.com>
2025-05-06 14:47:23 -0300

    feature: começando a adicionar a rota de categorias para navegar por id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { CategoriaComponent } from './components/categoria/categoria.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'categorias',component:CategoriaComponent}
+    {path:'categorias',component:CategoriaComponent},
+    {path:'categorias/:id',component:CategoriaComponent}
 ];
