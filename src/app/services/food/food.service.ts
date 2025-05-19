32d8f01fb4b2b13c1cdd3d4de523e14537b33044
mazeus <mateusfrazao12345@gmail.com>
2025-05-18 22:41:47 -0300

    modificando

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -5,20 +5,19 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
- private apiUrl = 'http://localhost:3000/produtos' //Passando o endpoint da api
-  constructor(private http: HttpClient) { }
-  
-  getProdutos() :Observable<Produto[]>{ 
-    return this.http.get<Produto[]>(this.apiUrl)
+  private apiUrl = 'http://localhost:3000/produtos'; //Passando o endpoint da api
+  constructor(private http: HttpClient) {}
+
+  getProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(this.apiUrl);
   }
 
   getProdutosEmPromocao(): Observable<Produto[]> {
     return this.getProdutos().pipe(
-      map(produtos => produtos.filter(produto => produto.promocao === true))
+      map((produtos) => produtos.filter((produto) => produto.promocao === true))
     );
   }
-  
 }
