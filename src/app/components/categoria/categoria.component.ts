4ab1139ba8415ea2be2a15fda2cc24444db8c8a7
mazeus <mateusfrazao12345@gmail.com>
2025-05-20 13:53:37 -0300

    finalizando o metodo do category component

--- src/app/components/categoria/categoria.component.ts
+++ src/app/components/categoria/categoria.component.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  Input,
+  SimpleChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriaProdutoService } from '../../services/categoriaProduto/categoria-produto.service';
 import { Categoria } from '../../Interfaces/CategoriaProduto';
-import { CardProdutoComponent } from "../card-produto/card-produto.component";
-import { CategoriaItensComponent } from "../categoria-itens/categoria-itens.component";
+import { CardProdutoComponent } from '../card-produto/card-produto.component';
+import { CategoriaItensComponent } from '../categoria-itens/categoria-itens.component';
+import { Produto } from '../../Interfaces/Produto';
+import { FoodService } from '../../services/food/food.service';
 
 @Component({
   selector: 'app-categoria',
-  standalone:true,
-  imports: [
-    CommonModule,
-    CategoriaItensComponent
-],
+  standalone: true,
+  imports: [CommonModule, CategoriaItensComponent],
   templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.css'
+  styleUrl: './categoria.component.css',
 })
-export class CategoriaComponent implements OnInit {
-     categoriaProduto: Categoria[] = []
+export class CategoriaComponent implements OnChanges, OnInit {
+  @Input() categoria: string[] = [];
+  allProducts: Produto[] = [];
+  produtos: Produto[] = [];
 
-     constructor( private categoriaService: CategoriaProdutoService){}
-     ngOnInit(): void {
-       this.categoriaService.getCategoriaProduto().subscribe((dado)=>{
-        this.categoriaProduto = dado;
-        console.log(dado)
-       })
-     }
+  constructor(private foodService: FoodService) {}
+
+  ngOnInit(): void {
+    this.foodService.getProdutos().subscribe((dadoProduto) => {
+      this.allProducts = dadoProduto;
+    });
+  }
+
+  ngOnChanges(): void {
+    this.produtos = this.allProducts.filter(
+      (p) => p.category === this.categoria
+    );
+  }
 }
