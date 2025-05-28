a8c594b6a7c7980736fc6040cec18d2d026f924d
mazeus <mateusfrazao12345@gmail.com>
2025-05-28 12:41:54 -0300

    organizando o caminho das rotas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from './routers/app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes), 
-     provideClientHydration(withEventReplay()),
-     provideHttpClient()
-    ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+  ],
 };
